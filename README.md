d5a84f2e7a59c0574e179a00323ef5393827b80c
Kevin Zepeda <kevin@floups.com>
2020-07-01 15:31:37 -0500

    Variables lesson

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Learn__go
-This repo contain a freecodecamp lessons playground
+This repo contain a freecodecamp lessons
 
 ## Topics
-0. Hello
-  - Firts steps into GO
-1. Variables
-  - Declaration
-  - Redeclaration and Shadowing
-  - Visibility
-  - Naming conventions
-  - Type conversions
+#### 0. Hello
+        - Firts steps into GO
+#### 1. Variables
+        - Declaration
+        - Redeclaration and Shadowing
+        - Visibility
+        - Naming conventions
+        - Type conversions
---
+++ Varaibles/vars.go
@@ -0,0 +1,56 @@
+package main
+
+import(
+    "fmt"
+)
+// Global vars
+var jp int = 21
+
+
+
+func main() {
+  // -------------------- Declaration---------------------------------
+  // var [Name of variable] [Type of variable]
+  // print value (%v) and type (%T) of this variable
+  // This is another format to declaration name := [value]
+  // the vars have to be used 
+  var number_name int
+  number_name = 42
+  fmt.Printf("%v",number_name)
+  var j float32 = 75
+  fmt.Printf("%v,%T",j,j)
+
+  // Another Declaration
+  i := 42  // [name of variable] := [Value]
+  // With this format can't redeclare variable
+  fmt.Printf("%T",i)
+
+  //Var blocks
+  // This blocks its only for more cleaning code
+  var (
+    name string = "Kevin"
+    fake_age int = 25
+  )
+
+  var (
+    another_block string = "This random message"
+    conuter_average float64 = 9.9
+  )
+
+
+  fmt.Printf("%v",name)
+  fmt.Printf("%v",fake_age)
+  fmt.Printf("%v",another_block)
+  fmt.Printf("%v",conuter_average)
+  fmt.Println("")
+
+  // ----------------Redeclaration --------------------------------
+  // We can redeclare variables if variable are not in the same block
+  // The format to redeclare is -> var name_var [type]
+  // Not this -> name_var := [value]
+  // In this example "jp" is global var and print and redeclare intro main block
+  fmt.Println(jp)
+  var jp int = 32
+  fmt.Println(jp)
+
+}
